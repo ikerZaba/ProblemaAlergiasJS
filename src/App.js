0389466228086560f7df7ba5548bf3e2791e29e6
Iker Zabalegui <124686535+ikerZaba@users.noreply.github.com>
2025-02-20 11:13:37 +0000

    tabla de alergenos y cambios de estilo

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 
 export default function App() {
   const alergias = [
-    "los huevos",
-    " los cacahuetes",
-    "el marisco",
-    "las fresas",
-    " los tomates",
-    "el chocolate",
-    "el polen",
-    "los gatos",
+    "los Huevos",
+    "los Cacahuetes",
+    "el Marisco",
+    "las Fresas",
+    "los Tomates",
+    "el Chocolate",
+    "el Polen",
+    "los Gatos",
   ];
   const [inputValue, setInputValue] = useState("");
   const [resultado, setResultado] = useState("");
@@ -28,7 +28,7 @@ export default function App() {
       var i = 0;
       while (scoreAlergias > 0) {
         if (scoreAlergias % 2 == 1) {
-          if (i == 0) {
+          if (alergiasCalculadas.localeCompare("Eres alérgico a") == 0) {
             alergiasCalculadas = alergiasCalculadas.concat(": ", alergias[i]);
           } else {
             alergiasCalculadas = alergiasCalculadas.concat(", ", alergias[i]);
@@ -56,6 +56,50 @@ export default function App() {
         Calcular
       </button>
       {<p class="parrafoResultado">{resultado}</p>}
+      <div class="listaAlergias">
+        <table class="tablaAlergias">
+          <thead>
+            <tr>
+              <th>Alérgeno</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{alergias[0].split(" ")[1]}</td>
+              <td>{2 ** 0}</td>
+            </tr>
+            <tr>
+              <td>{alergias[1].split(" ")[1]}</td>
+              <td>{2 ** 1}</td>
+            </tr>
+            <tr>
+              <td>{alergias[2].split(" ")[1]}</td>
+              <td>{2 ** 2}</td>
+            </tr>
+            <tr>
+              <td>{alergias[3].split(" ")[1]}</td>
+              <td>{2 ** 3}</td>
+            </tr>
+            <tr>
+              <td>{alergias[4].split(" ")[1]}</td>
+              <td>{2 ** 4}</td>
+            </tr>
+            <tr>
+              <td>{alergias[5].split(" ")[1]}</td>
+              <td>{2 ** 5}</td>
+            </tr>
+            <tr>
+              <td>{alergias[6].split(" ")[1]}</td>
+              <td>{2 ** 6}</td>
+            </tr>
+            <tr>
+              <td>{alergias[7].split(" ")[1]}</td>
+              <td>{2 ** 7}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
